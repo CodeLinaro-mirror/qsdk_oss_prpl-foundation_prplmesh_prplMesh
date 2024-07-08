ed054f8f7f3187d8883e9d433d1462340ea87dc7
HENRIQUES Nicolas <prplmesh.opensources@sagemcom.com>
2024-07-08 15:45:40 +0200

    tlvf: tid to link mapping policy refacto
    Signed-off-by: HENRIQUES Nicolas <prplmesh.opensources@sagemcom.com>

--- framework/tlvf/src/src/CmduMessageRx.cpp
+++ framework/tlvf/src/src/CmduMessageRx.cpp
@@ -120,7 +120,7 @@
 #include <tlvf/wfa_map/tlvSteeringPolicy.h>
 #include <tlvf/wfa_map/tlvSteeringRequest.h>
 #include <tlvf/wfa_map/tlvSupportedService.h>
-#include <tlvf/wfa_map/tlvTIDtoLinkMappingPolicy.h>
+#include <tlvf/wfa_map/tlvTidToLinkMappingPolicy.h>
 #include <tlvf/wfa_map/tlvTimestamp.h>
 #include <tlvf/wfa_map/tlvTransmitPowerLimit.h>
 #include <tlvf/wfa_map/tlvTriggerChannelSwitchAnnouncement.h>
@@ -572,7 +572,7 @@ std::shared_ptr<BaseClass> CmduMessageRx::parseNextTlv(wfa_map::eTlvTypeMap tlv_
         return msg.addClass<wfa_map::tlvSpatialReuseReport>();
     }
     case (wfa_map::eTlvTypeMap::TLV_TID_TO_LINK_MAPPING_POLICY): {
-        return msg.addClass<wfa_map::tlvTIDtoLinkMappingPolicy>();
+        return msg.addClass<wfa_map::tlvTidToLinkMappingPolicy>();
     }
     }
     LOG(FATAL) << "Unknown TLV type: " << unsigned(tlv_type);
