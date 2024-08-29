42bc9e784b9dc9bf0ce69ea280b4e0c58d28dba4
bmanoharan <bmanoharan@maxlinear.com>
2024-08-29 11:27:00 +0200

    primary VLAN ID
        (cherry picked from commit 4ae02168c248f3d1493f9581e7c426df705d68cc)

--- tools/device/generic.py
+++ tools/device/generic.py
@@ -45,7 +45,7 @@ class GenericDevice():
     """The time (in seconds) the device needs to initialize when it boots
     for the first time after flashing a new image."""
 
-    configuration_initialization_time = 30
+    configuration_initialization_time = 15
     """The time (in seconds) the device needs to initialize after configuring it"""
 
     bootloader_prompt = "=> "
