7b6eac5ca2cc520a05681dbf281c43cfb854eaeb
Maarten De Decker <maarten.dedecker@mind.be>
2024-09-04 21:33:11 +0200

    WIP: add freedom build
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -102,7 +102,7 @@ main() {
 
     eval set -- "$OPTS"
 
-    SUPPORTED_TARGETS="turris-omnia haze urx_osp"
+    SUPPORTED_TARGETS="turris-omnia haze urx_osp freedom"
 
     while true; do
         case "$1" in
@@ -138,6 +138,9 @@ main() {
         urx_osp)
             TARGET_SYSTEM=mxl_x86_osp_tb341
             ;;
+        freedom)
+            TARGET_SYSTEM=qca_ipq95xx
+            ;;
         *)
             err "Unknown target device: $TARGET_DEVICE"
             info "Currently supported targets are:"
@@ -156,6 +159,10 @@ main() {
         dbg "OSP platform, build on prplos UPDK 9.1.50 + pWHM 5.34.0"
         OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
         OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
+    elif [[ "freedom" == "$TARGET_DEVICE" ]] ; then
+        dbg "Freedom platform prplOS + pWHM 6.26.1"
+        OPENWRT_TOOLCHAIN_VERSION='4578ad1e7c65cfaa3f993e7d664c0f8385f46dfc'
+        OPENWRT_VERSION='4578ad1e7c65cfaa3f993e7d664c0f8385f46dfc'
     else
         dbg "Building on prplOS-next"
     fi
