480de888eeec3f5e84b9d6dc551be68d57d6ab19
Volodymyr Pavlenko <v.pavlenko@inango-systems.com>
2024-10-16 14:53:28 +0300

    controller: nbapi: remove default Device
    We have issue that dummy device is created on firstboot.
    The problem stems from the odl default configuration to
    have default Device.1
    We don't need it, because prplmesh creates local agent
    Device by itself.
    
    Signed-off-by: Volodymyr Pavlenko <v.pavlenko@inango-systems.com>

--- controller/src/beerocks/master/db/db.cpp
+++ controller/src/beerocks/master/db/db.cpp
@@ -260,6 +260,11 @@ const std::shared_ptr<Agent> db::get_local_agent()
 
 std::shared_ptr<Agent> db::add_gateway(const sMacAddr &mac)
 {
+    if (mac == network_utils::ZERO_MAC) {
+        LOG(ERROR) << "mac supplied for add_gateway is zero_mac";
+        return {};
+    }
+
     auto agent = m_agents.add(mac);
 
     agent->is_gateway = true;
@@ -306,6 +311,7 @@ std::shared_ptr<Agent> db::add_agent(const sMacAddr &mac, const sMacAddr &parent
         LOG(ERROR) << "mac supplied for add_agent is zero_mac";
         return {};
     }
+
     auto agent = m_agents.add(mac);
     if (!agent) {
         LOG(ERROR) << "Failed to add Agent " << mac;
