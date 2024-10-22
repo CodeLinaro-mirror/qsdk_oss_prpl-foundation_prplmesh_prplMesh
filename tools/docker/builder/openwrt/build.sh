558e2c709110b29d11609c0add9c67cef88db28f
Maarten De Decker <maarten.dedecker@mind.be>
2024-10-22 14:31:22 +0200

    Freedom: change prplOS base to 23.05
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -160,9 +160,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
         OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
     elif [[ "freedom" == "$TARGET_DEVICE" ]] ; then
-        dbg "Freedom platform, prplOS mainline + pWHM latest + endpoints"
-        OPENWRT_TOOLCHAIN_VERSION='f039941c153f774188107cfc98850703e4725322'
-        OPENWRT_VERSION='f039941c153f774188107cfc98850703e4725322'
+        dbg "Freedom platform, prplOS 23.05"
+        OPENWRT_TOOLCHAIN_VERSION='e333cb1c7a7d92fbb81cf906b031f43a790e69d2'
+        OPENWRT_VERSION='e333cb1c7a7d92fbb81cf906b031f43a790e69d2'
     else
         dbg "Building on prplOS-next"
     fi
