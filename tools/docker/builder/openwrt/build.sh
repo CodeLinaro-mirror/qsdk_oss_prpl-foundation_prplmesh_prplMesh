6c6cfa3b3823ce9d16a0c01ea7ef8a02a48732fe
Maarten De Decker <maarten.dedecker@mind.be>
2024-10-22 20:37:54 +0200

    Freedom: change prplOS to mainline-3.2
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -160,9 +160,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
         OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
     elif [[ "freedom" == "$TARGET_DEVICE" ]] ; then
-        dbg "Freedom platform, prplOS mainline + pWHM latest + endpoints"
-        OPENWRT_TOOLCHAIN_VERSION='f039941c153f774188107cfc98850703e4725322'
-        OPENWRT_VERSION='f039941c153f774188107cfc98850703e4725322'
+        dbg "Freedom platform, prplOS mainline 3.2 + vlanid patch + addvapintf patch + 6GHz ep remove + hostapd segf fix"
+        OPENWRT_TOOLCHAIN_VERSION='329f8dfc50edfe28dda42e216e1537ecec5172c3'
+        OPENWRT_VERSION='329f8dfc50edfe28dda42e216e1537ecec5172c3'
     else
         dbg "Building on prplOS-next"
     fi
