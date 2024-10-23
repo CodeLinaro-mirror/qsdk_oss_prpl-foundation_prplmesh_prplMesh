f50a5be40c2eb200d2969e95d34b50044c2c4aaa
Maarten De Decker <maarten.dedecker@mind.be>
2024-10-23 10:35:15 +0200

    Freedom: change to mainline 3.1
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -157,12 +157,12 @@ main() {
         OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
     elif [[ "urx_osp" == "$TARGET_DEVICE" ]] ; then
         dbg "OSP platform, build on prplos UPDK 9.1.50 + pWHM 5.34.0"
-        OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
-        OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
+        OPENWRT_TOOLCHAIN_VERSION='f039941c153f774188107cfc98850703e4725322'
+        OPENWRT_VERSION='f039941c153f774188107cfc98850703e4725322'
     elif [[ "freedom" == "$TARGET_DEVICE" ]] ; then
-        dbg "Freedom platform, prplOS mainline 3.2"
-        OPENWRT_TOOLCHAIN_VERSION='179a3309181fc4c97951de6570fd83cd6f59ec9c'
-        OPENWRT_VERSION='179a3309181fc4c97951de6570fd83cd6f59ec9c'
+        dbg "Freedom platform, prplOS mainline 3.1"
+        OPENWRT_TOOLCHAIN_VERSION='f039941c153f774188107cfc98850703e4725322'
+        OPENWRT_VERSION='f039941c153f774188107cfc98850703e4725322'
     else
         dbg "Building on prplOS-next"
     fi
