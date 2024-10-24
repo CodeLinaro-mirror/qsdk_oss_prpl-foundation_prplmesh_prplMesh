3e00a6e3abecb22be6bd2ab3850e98368e79b59c
Maarten De Decker <maarten.dedecker@mind.be>
2024-10-24 13:07:58 +0200

    Freedom: test adding extra vaps and prio modules
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -160,9 +160,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
         OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
     elif [[ "freedom" == "$TARGET_DEVICE" ]] ; then
-        dbg "Freedom platform, prplOS mainline + pWHM latest + endpoints"
-        OPENWRT_TOOLCHAIN_VERSION='f039941c153f774188107cfc98850703e4725322'
-        OPENWRT_VERSION='f039941c153f774188107cfc98850703e4725322'
+        dbg "Freedom platform, prplOS mainline + added VAPs + prio"
+        OPENWRT_TOOLCHAIN_VERSION='6c132fbd1b39c071109c19aba45d15a6878b0dc6'
+        OPENWRT_VERSION='6c132fbd1b39c071109c19aba45d15a6878b0dc6'
     else
         dbg "Building on prplOS-next"
     fi
