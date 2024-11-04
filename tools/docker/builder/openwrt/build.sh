8b783a244fb34f5f0a76f4ea60b5f9e02eeff9fd
Volodymyr Pavlenko <v.pavlenko@inango-systems.com>
2024-11-04 12:26:55 +0200

    Add mainline 3.2
    Signed-off-by: Volodymyr Pavlenko <v.pavlenko@inango-systems.com>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -152,17 +152,17 @@ main() {
     esac
 
     if [[ "haze" == "$TARGET_DEVICE" ]] ; then
-        dbg "Haze platform, build on prplos master + pWHM 5.34.0"
-        OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
-        OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
+        dbg "Haze platform, build on prplos mainline-3.2"
+        OPENWRT_TOOLCHAIN_VERSION='7bd8640a26a751b33c354a15517c5ce6f6a8d4b3'
+        OPENWRT_VERSION='7bd8640a26a751b33c354a15517c5ce6f6a8d4b3'
     elif [[ "urx_osp" == "$TARGET_DEVICE" ]] ; then
         dbg "OSP platform, build on prplos UPDK 9.1.50 + pWHM 5.34.0"
         OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
         OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
     elif [[ "freedom" == "$TARGET_DEVICE" ]] ; then
-        dbg "Freedom platform, prplOS mainline + pWHM latest + endpoints"
-        OPENWRT_TOOLCHAIN_VERSION='f039941c153f774188107cfc98850703e4725322'
-        OPENWRT_VERSION='f039941c153f774188107cfc98850703e4725322'
+        dbg "Freedom platform, prplOS mainline-3.2 + vlanid patch + new networklayout"
+        OPENWRT_TOOLCHAIN_VERSION='7bd8640a26a751b33c354a15517c5ce6f6a8d4b3'
+        OPENWRT_VERSION='7bd8640a26a751b33c354a15517c5ce6f6a8d4b3'
     else
         dbg "Building on prplOS-next"
     fi
