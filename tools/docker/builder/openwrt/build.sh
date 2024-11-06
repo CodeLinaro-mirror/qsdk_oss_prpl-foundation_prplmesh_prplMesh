7ae059e8d539432d3dfb1101ed6fcb2777d69413
Volodymyr Pavlenko <v.pavlenko@inango-systems.com>
2024-11-06 16:31:42 +0200

    TESTING: mainline-3.2 + WHM v6.44.16
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>
    Signed-off-by: Volodymyr Pavlenko <v.pavlenko@inango-systems.com>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -156,9 +156,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
         OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
     elif [[ "urx_osp" == "$TARGET_DEVICE" ]] ; then
-        dbg "OSP platform, build on prplos UPDK 9.1.50 + pWHM 5.34.0"
-        OPENWRT_TOOLCHAIN_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
-        OPENWRT_VERSION='538cd93744bc16f4b826143d959b548cf572df4a'
+        dbg "OSP platform, prplOS mainline 3.2 + WHM upstep v6.44.16"
+        OPENWRT_TOOLCHAIN_VERSION='b4c23ce845c09057e130416537a6bb125b66f255'
+        OPENWRT_VERSION='b4c23ce845c09057e130416537a6bb125b66f255'
     elif [[ "freedom" == "$TARGET_DEVICE" ]] ; then
         dbg "Freedom platform, prplOS mainline + pWHM latest + endpoints"
         OPENWRT_TOOLCHAIN_VERSION='f039941c153f774188107cfc98850703e4725322'
