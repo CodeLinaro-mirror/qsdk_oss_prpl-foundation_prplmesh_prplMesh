ceb713e83419aac439b8230091bfa1d797e3f725
Elon Nguyen <elon_nguyen@vn.gemteks.com>
2025-01-24 10:15:10 +0700

    bwl: Add parameter in the update_vap_credentials function, file ap_wlan_hal_whm.cpp (PPW-334)
    Problem:  After onboarding, clients cannot connect to the Easymesh agent AP when using WPA3 authentication.
    
    Cause: The wifi credentials for WPA3 (SAEPassphrase) is not updated during AP Auto-configuration process.
    
    Solution: Modify the update_vap_credentials function to set the  WPA3 password (SAEPassphrase) .
    
    Issue: PPW-334: The Easymesh agent does not synchronize the WiFi password for WPA3 authentication
    Signed-off-by: Elon Nguyen <elon_nguyen@vn.gemteks.com>
    (cherry picked from commit fcdba496aeee9a105ce5a62dc9c34249a69b0a01)
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- common/beerocks/bwl/whm/ap_wlan_hal_whm.cpp
+++ common/beerocks/bwl/whm/ap_wlan_hal_whm.cpp
@@ -588,6 +588,7 @@ bool ap_wlan_hal_whm::update_vap_credentials(
         } else {
             new_obj.add_child("EncryptionMode", encryption_mode);
             new_obj.add_child("KeyPassPhrase", bss_info_conf.network_key);
+            new_obj.add_child("SAEPassphrase", bss_info_conf.network_key);
         }
         ret = m_ambiorix_cl.update_object(wifi_ap_sec_path, new_obj);
 
