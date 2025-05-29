0e83b0e949c71fe28fb1aab3af71dc44a20e9c09
Volodymyr Pavlenko <pavlenko.vv@pm.me>
2025-05-29 17:07:59 +0300

    test
    Signed-off-by: Volodymyr Pavlenko <pavlenko.vv@pm.me>

--- agent/src/beerocks/fronthaul_manager/monitor/monitor.cpp
+++ agent/src/beerocks/fronthaul_manager/monitor/monitor.cpp
@@ -205,8 +205,11 @@ bool Monitor::thread_init()
                         "measurements for all clients";
         clients_measuremet_mode = bpl::eClientsMeasurementMode::ENABLE_ALL;
     }
+
+    LOG(DEBUG) << "VOLODYMYR | clients_measuremet_mode=" << clients_measuremet_mode;
+
     mon_db.set_clients_measuremet_mode(
-        (monitor_db::eClientsMeasurementMode)clients_measuremet_mode);
+        (monitor_db::eClientsMeasurementMode)bpl::eClientsMeasurementMode::ENABLE_ALL);
 #endif /* FEATURE_PRE_ASSOCIATION_STEERING */
     bool radio_stats_enable;
     if (!beerocks::bpl::cfg_get_radio_stats_enable(radio_stats_enable)) {
@@ -921,6 +924,8 @@ bool Monitor::update_sta_stats(const std::chrono::steady_clock::time_point &time
             // This is a potentially long running operation.
             // If we haven't finished iterating on all stations, stop here and continue on next
             // method call from this point on.
+            LOG(WARNING)
+                << "update_sta_stats: long running operation, stop iterating on all stations";
             return true;
         }
 
