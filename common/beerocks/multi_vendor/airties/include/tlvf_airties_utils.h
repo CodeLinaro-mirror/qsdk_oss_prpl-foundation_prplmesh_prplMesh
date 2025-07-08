565a08f0940a879bea7fafeef36e3c6a3c0dde9a
Sundram Patel <Sundram.p@tataelxsi.co.in>
2025-07-08 15:59:33 +0530

    tlvf_airties_utils: assign_unique_port_id(): convert interface names to unique port IDs
    Key Features:
        - Read Ethernet interface names from Device.Ethernet.Interface.*.Alias
        - Assign each interface a unique numeric port ID
        - Used for populating Airties Ethernet Stats TLV and Airties Ethernet Interface TLV.
    
    OSPv2/WNC Port ID assignment results for reference:
    OSPv2:				WNC:
    ETH0_1 -> port_id:1		LAN1 -> port_id:1
    ETH0_2 -> port_id:2		LAN2 -> port_id:2
    ETH0_3 -> port_id:3		LAN3 -> port_id:3
    ETH0_4 -> port_id:4		LAN4 -> port_id:4
    ETH0_5 -> port_id:5
    
    Reference: PPW-584
    Signed-off-by: Sundram Patel <Sundram.p@tataelxsi.co.in>
    Signed-off-by: Frederik Van Bogaert <frederik.vanbogaert@mind.be>

--- common/beerocks/multi_vendor/airties/include/tlvf_airties_utils.h
+++ common/beerocks/multi_vendor/airties/include/tlvf_airties_utils.h
@@ -55,6 +55,21 @@ public:
     get_counters_info(std::shared_ptr<airties::tlvAirtiesEthernetStats> &tlvAirtiesEthStats);
     static bool get_all_counters_info(
         std::shared_ptr<airties::tlvAirtiesEthernetStatsallcntr> &tlvAirtiesEthStats);
+
+    /**
+     * @brief Assigns a unique port ID for the given interface name.
+     *
+     * This function ensures that each interface name is assigned a unique port ID.
+     * If the interface has already been assigned an ID, it returns the same ID.
+     *
+     * The function attempts to extract a numeric suffix from the interface name
+     * (e.g., "eth3" → 3 or "eth0" → 0, "eth0.1" -> 1, eth3-> 2) and uses it as the port ID if it's not already used.
+     * Otherwise, it assigns the next available ID starting from 1.
+     *
+     * @param interface_name The name of the interface (e.g., "eth1", "wan0").
+     * @return A unique port ID associated with the given interface name.
+     */
+    static uint8_t assign_unique_port_id(const std::string &alias_raw);
 };
 } // namespace airties
 
