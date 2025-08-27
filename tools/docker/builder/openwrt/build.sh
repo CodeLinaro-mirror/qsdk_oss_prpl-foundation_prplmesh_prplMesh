9c706eea39c87094045027b01376b31d8c1af3bb
Maarten De Decker <maarten.dedecker@mind.be>
2025-08-27 10:10:11 +0200

    CI: change prplOS version to mainline-23.05 August 25 2025
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -160,9 +160,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS mainline-23.05 (stable-v4.0 08/08/2025)"
-        OPENWRT_TOOLCHAIN_VERSION='9791b9ad6d5dd00f9aa1bbc54e2095492b46287f'
-        OPENWRT_VERSION='9791b9ad6d5dd00f9aa1bbc54e2095492b46287f'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS mainline-23.05 (stable-v4.0 25/08/2025)"
+        OPENWRT_TOOLCHAIN_VERSION='1343b2238a8f877ee7637a301931eb6c06cfbc14'
+        OPENWRT_VERSION='1343b2238a8f877ee7637a301931eb6c06cfbc14'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -202,8 +202,8 @@ VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
 # prplos mainline-23.05 (stable-v4.0)
-OPENWRT_TOOLCHAIN_VERSION='9791b9ad6d5dd00f9aa1bbc54e2095492b46287f'
-OPENWRT_VERSION='9791b9ad6d5dd00f9aa1bbc54e2095492b46287f'
+OPENWRT_TOOLCHAIN_VERSION='1343b2238a8f877ee7637a301931eb6c06cfbc14'
+OPENWRT_VERSION='1343b2238a8f877ee7637a301931eb6c06cfbc14'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
