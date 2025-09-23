525eaf8022ec17b2386b38b9424b0c55073dadc3
Maarten De Decker <maarten.dedecker@mind.be>
2025-09-23 21:13:10 +0200

    CI: bump prplOS version to mainline-23.05 2025-09-19
    This updates the UPDK for OSPv2 to 9.1.95.
    This allows using the flash layout introduced in this UPDK.
    
    Closes: PPM-3534
    
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -160,9 +160,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplos mainline-23.05, 11 sept 2025 (prplware-v4.0.1)"
-        OPENWRT_TOOLCHAIN_VERSION='8b82aba028174269755b82fdbc03ae77cd40265c'
-        OPENWRT_VERSION='8b82aba028174269755b82fdbc03ae77cd40265c'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS mainline-23.05_2025-09-19 UPDK-9.1.95"
+        OPENWRT_TOOLCHAIN_VERSION='a5811e5a3df01e6a78c43caa1d4fe793745351ed'
+        OPENWRT_VERSION='a5811e5a3df01e6a78c43caa1d4fe793745351ed'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -201,9 +201,9 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-# prplos mainline-23.05 - v4.0.1
-OPENWRT_TOOLCHAIN_VERSION='8b82aba028174269755b82fdbc03ae77cd40265c'
-OPENWRT_VERSION='8b82aba028174269755b82fdbc03ae77cd40265c'
+# prplos mainline-23.05_2025-09-19
+OPENWRT_TOOLCHAIN_VERSION='a5811e5a3df01e6a78c43caa1d4fe793745351ed'
+OPENWRT_VERSION='a5811e5a3df01e6a78c43caa1d4fe793745351ed'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
