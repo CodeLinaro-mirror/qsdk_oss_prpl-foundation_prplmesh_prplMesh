66b51095724756518b3b0f78d58aaf4bddea9918
Dmytro Puz <d.puz@inango-systems.com>
2025-10-08 12:56:03 +0200

    ci: certification: set Agent DM MultiAPProfile=1 in R1 setups to preserve tests
    The default Agent Multi-AP profile now comes from the Agent DM
    (X_PRPLWARE-COM_Agent.Configuration.MultiAPProfile) and defaults to R2 in
    code. Our R1 certification topologies assume R1 behavior and previously relied
    on the old search “send twice” workaround and implicit defaults.
    
    Explicitly configure the Agent to R1 in all R1 certification
    device setup scripts to avoid breaking existing testbeds and to keep controller
    discovery compatible with R1 controllers.
    
    PPM-3434.
    
    Signed-off-by: Dmytro Puz <d.puz@inango-systems.com>

--- ci/configuration/certification/R1/axepoint.sh
+++ ci/configuration/certification/R1/axepoint.sh
@@ -102,6 +102,8 @@ else
   # Prplmesh agent is running, configure it over the bus
   echo "Setting prplMesh BackhaulWireInterface over DM"
   ba-cli X_PRPLWARE-COM_Agent.Configuration.BackhaulWireInterface="eth1"
+  echo "Setting prplMesh MultiAPProfile over DM"
+  ba-cli X_PRPLWARE-COM_Agent.Configuration.MultiAPProfile=1
 fi
 
 # For now there is no way to disable the firewall (see PCF-590).
