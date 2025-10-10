7052bd4635256a0f30b11ce9c4b4da27d0efaa04
Saran Mahalingam <saran.mahalingam@tataelxsi.co.in>
2025-10-10 09:29:50 +0530

    nbapi:DM for trigger MLDUnit Description: MLDUnit is added as part of prplMesh controller northbound API for below AccessPoint DM X_PRPLWARE-COM_WiFiController.Network.AccessPoint.{i}.MLDUnit
    Closes: PPM-3564
    
    Signed-off-by: Saran Mahalingam <saran.mahalingam@tataelxsi.co.in>

--- controller/nbapi/on_action.cpp
+++ controller/nbapi/on_action.cpp
@@ -365,6 +365,28 @@ amxd_status_t access_point_commit(amxd_object_t *object, amxd_function_t *func, 
                              << " missing value for network key.";
                 continue;
             }
+
+            // Handle MLDUnit configuration
+            int8_t mld_unit = amxd_object_get_int8_t(access_point_inst, "MLDUnit", nullptr);
+            if (mld_unit != -1) {
+                // Validate MLDUnit is in valid range [0, 15]
+                if (mld_unit >= 0 && mld_unit <= 15) {
+                    son::wireless_utils::sMldInfoConf mld_info;
+                    mld_info.ssid = bss_info.ssid;
+                    mld_info.str  = true;
+                    mld_info.nstr = false;
+                    mld_info.emlsr = false;
+                    mld_info.emlmr = false;
+                    std::string mld_id = std::to_string(mld_unit);
+                    g_database->add_mld_info_configuration(mld_info, mld_id);
+                    LOG(DEBUG) << "Added MLD configuration for SSID: " << bss_info.ssid
+                       << " with MLDUnit: " << static_cast<int>(mld_unit);
+                } else {
+                    LOG(WARNING) << "Invalid MLDUnit value " << static_cast<int>(mld_unit)
+                       << " for SSID: " << bss_info.ssid
+                       << ". Must be in range [0, 15] or -1 for disabled.";
+                }
+            }
             LOG(DEBUG) << "Add bss info configration for AP with ssid: " << bss_info.ssid
                        << " and operating classes: " << bss_info.operating_class;
             g_database->add_bss_info_configuration(bss_info);
