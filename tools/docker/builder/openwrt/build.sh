c315f0473cfbf1a7e423b21a5a6192c271f1221c
Lu Dai <lu.dai@mind.be>
2025-10-23 14:02:47 +0200

    CI: bump prplOS version to mainline-23.05 with pwhm cherry pick
    Signed-off-by: Lu Dai <lu.dai@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -160,9 +160,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS mainline-23.05_2025-09-19 UPDK-9.1.95"
-        OPENWRT_TOOLCHAIN_VERSION='a5811e5a3df01e6a78c43caa1d4fe793745351ed'
-        OPENWRT_VERSION='a5811e5a3df01e6a78c43caa1d4fe793745351ed'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS mainline-23.05 with pwhm cherry pick"
+        OPENWRT_TOOLCHAIN_VERSION='3c962742a279d126f73477f1eb5b25e2e379acda'
+        OPENWRT_VERSION='3c962742a279d126f73477f1eb5b25e2e379acda'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -201,9 +201,9 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-# prplos mainline-23.05_2025-09-19
-OPENWRT_TOOLCHAIN_VERSION='a5811e5a3df01e6a78c43caa1d4fe793745351ed'
-OPENWRT_VERSION='a5811e5a3df01e6a78c43caa1d4fe793745351ed'
+# prplos mainline-23.05_2025 with pwhm cherry pick
+OPENWRT_TOOLCHAIN_VERSION='3c962742a279d126f73477f1eb5b25e2e379acda'
+OPENWRT_VERSION='3c962742a279d126f73477f1eb5b25e2e379acda'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
