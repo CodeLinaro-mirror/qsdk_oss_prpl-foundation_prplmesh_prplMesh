04ba5845217ecfe9b60be79d797a3e8790e99b13
sushmitha <sushmitha.s_ext@softathome.com>
2025-10-29 13:53:13 +0530

    prplMesh : Controller : fix controller-only mode
    Problem:
    Log spam in beerocks_agent.log due to a dummy autoconfiguration task.
    
    Fix:
    Disabled the dummy autoconfiguration task in Controller-only mode to prevent log spam.
    
    Closes PPW-546
    
    Signed-off-by: sushmitha <sushmitha.s_ext@softathome.com>

--- agent/src/beerocks/slave/tasks/topology_task.cpp
+++ agent/src/beerocks/slave/tasks/topology_task.cpp
@@ -13,6 +13,7 @@
 #include "multi_vendor.h"
 
 #include <bcl/network/network_utils.h>
+#include <bpl/bpl_cfg.h>
 
 #include <beerocks/tlvf/beerocks_message_backhaul.h>
 
@@ -110,8 +111,9 @@ void TopologyTask::work()
         send_topology_notification();
     }
 
-    // Don't send topology discovery if the Controller hasn't been discovered.
-    if (db->controller_info.bridge_mac == network_utils::ZERO_MAC) {
+    // Don't send topology discovery if the Controller hasn't been discovered and in Controller Only Mode
+    if ((db->controller_info.bridge_mac == network_utils::ZERO_MAC) &&
+        (db->device_conf.management_mode != BPL_MGMT_MODE_MULTIAP_CONTROLLER)) {
         return;
     }
 
