f8cc22307bf760eb74c52a80e232c166ac63a15f
Maarten De Decker <maarten.dedecker@mind.be>
2025-11-10 23:21:52 +0100

    WIP: add TLVF changes for AKM24
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- framework/tlvf/AutoGenerated/include/tlvf/WSC/eWscAuth.h
+++ framework/tlvf/AutoGenerated/include/tlvf/WSC/eWscAuth.h
@@ -14,38 +14,38 @@
 #define _TLVF_WSC_EWSCAUTH_H_
 
 #include <cstddef>
-#include <ostream>
 #include <stdint.h>
 #include <tlvf/swap.h>
+#include <ostream>
 
 namespace WSC {
 
-enum eWscAuth : uint16_t {
-    WSC_AUTH_OPEN      = 0x1,
-    WSC_AUTH_WPAPSK    = 0x2,
-    WSC_AUTH_SHARED    = 0x4,
-    WSC_AUTH_WPA       = 0x8,
-    WSC_AUTH_WPA2      = 0x10,
-    WSC_AUTH_WPA2PSK   = 0x20,
-    WSC_AUTH_SAE       = 0x40,
+enum eWscAuth: uint16_t {
+    WSC_AUTH_OPEN = 0x1,
+    WSC_AUTH_WPAPSK = 0x2,
+    WSC_AUTH_SHARED = 0x4,
+    WSC_AUTH_WPA = 0x8,
+    WSC_AUTH_WPA2 = 0x10,
+    WSC_AUTH_WPA2PSK = 0x20,
+    WSC_AUTH_SAE = 0x40,
     WSC_AUTH_SAE_AKM24 = 0x100,
-    WSC_AUTH_RSN       = 0x200,
-    WSC_AUTH_INVALID   = 0xffff,
+    WSC_AUTH_RSN = 0x200,
+    WSC_AUTH_INVALID = 0xffff,
 };
 // Enum AutoPrint generated code snippet begining- DON'T EDIT!
 // clang-format off
 static const char *eWscAuth_str(eWscAuth enum_value) {
     switch (enum_value) {
-    case WSC_AUTH_OPEN:    return "WSC_AUTH_OPEN";
-    case WSC_AUTH_WPAPSK:  return "WSC_AUTH_WPAPSK";
-    case WSC_AUTH_SHARED:  return "WSC_AUTH_SHARED";
-    case WSC_AUTH_WPA:     return "WSC_AUTH_WPA";
-    case WSC_AUTH_WPA2:    return "WSC_AUTH_WPA2";
-    case WSC_AUTH_WPA2PSK: return "WSC_AUTH_WPA2PSK";
-    case WSC_AUTH_SAE:     return "WSC_AUTH_SAE";
-    case WSC_AUTH_SAE_AKM24:     return "WSC_AUTH_SAE"; // TODO: AKM Handling
-    case WSC_AUTH_RSN:     return "WSC_AUTH_RSN";
-    case WSC_AUTH_INVALID: return "WSC_AUTH_INVALID";
+    case WSC_AUTH_OPEN:      return "WSC_AUTH_OPEN";
+    case WSC_AUTH_WPAPSK:    return "WSC_AUTH_WPAPSK";
+    case WSC_AUTH_SHARED:    return "WSC_AUTH_SHARED";
+    case WSC_AUTH_WPA:       return "WSC_AUTH_WPA";
+    case WSC_AUTH_WPA2:      return "WSC_AUTH_WPA2";
+    case WSC_AUTH_WPA2PSK:   return "WSC_AUTH_WPA2PSK";
+    case WSC_AUTH_SAE:       return "WSC_AUTH_SAE";
+    case WSC_AUTH_SAE_AKM24: return "WSC_AUTH_SAE_AKM24";
+    case WSC_AUTH_RSN:       return "WSC_AUTH_RSN";
+    case WSC_AUTH_INVALID:   return "WSC_AUTH_INVALID";
     }
     static std::string out_str = std::to_string(int(enum_value));
     return out_str.c_str();
@@ -54,6 +54,7 @@ inline std::ostream &operator<<(std::ostream &out, eWscAuth value) { return out 
 // clang-format on
 // Enum AutoPrint generated code snippet end
 
-}; // namespace WSC
+
+}; // close namespace: WSC
 
 #endif //_TLVF/WSC_EWSCAUTH_H_
