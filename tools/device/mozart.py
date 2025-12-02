61497cd9a3876c45d4b53b543083c47ea2d48939
Lu Dai <lu.dai@mind.be>
2025-12-02 12:04:17 +0100

    ci: r4: add mozart platform
    add mozart into r4 test loop;
    align the mozart configuration of r4 and boradfarm;
    
    Signed-off-by: Lu Dai <lu.dai@mind.be>

--- tools/device/mozart.py
+++ tools/device/mozart.py
@@ -13,8 +13,6 @@ import pexpect
 import pexpect.fdpexpect
 import pexpect.pxssh
 from device.prplos import GenericPrplOS
-from device.serial import SerialDevice
-from device.utils import ShellType
 
 
 class Mozart(GenericPrplOS):
@@ -24,60 +22,20 @@ class Mozart(GenericPrplOS):
     and 'ipaddr' should already be set in the bootloader.
     """
 
-    bootloader_prompt = r"MT7988> "
     """The u-boot prompt on the target."""
+    bootloader_prompt = r"MT7988> "
     boot_stop_sequence = "0"
-    """
-    fakereset is an alias in the deployed device u-boot environment
-    ramfsfile=openwrt-mediatek-filogic-arcadyan_mozart-initramfs.itb
-    fakereset=tftpboot ${loadaddr} ${ramfsfile}; bootm ${loadaddr}
-    """
-    bootloader_reboot_command = "run fakereset"
+    bootloader_reboot_command = "run boot_production"
 
     def upgrade_from_u_boot(self, shell: pexpect.fdpexpect.fdspawn):
-        """
-        Note: do nothing here
-        Mozart in boardfarm does not do real upgrade
-        """
         shell.sendline("")
         shell.expect(self.bootloader_prompt)
+        # Give the ethernet interfaces some time to initialize:
         time.sleep(10)
-        # do nothing
-
-    def reboot(self, serial_type: ShellType, stop_in_bootloader: bool = False):
-        """
-        Note: do load initramfs via tftp
-        Mozart in boardfarm do "upgrade" in this step
-        By sendline(bootloader_reboot_command)
-        """
-        with SerialDevice(self.baudrate, self.name,
-                          self.serial_prompt, expect_prompt_on_connect=False) as shell:
-            print("Reset board.")
-
-            if serial_type == ShellType.UBOOT:
-                if stop_in_bootloader:
-                    shell.sendline("reset")
-                else:
-                    shell.sendline(self.bootloader_reboot_command)
-            elif serial_type in [ShellType.PRPLOS, ShellType.RDKB, ShellType.LINUX_UNKNOWN]:
-                shell.sendline("reboot ; sleep 15 && echo force rebooting && reboot -f")
-            if stop_in_bootloader:
-                print("Device will be stopped in its bootloader.")
-                max_wait = 180  # total seconds to wait for boot menu
-                start_time = time.time()
-                boot_prompt_detected = False
-                while time.time() - start_time < max_wait:
-                    try:
-                        shell.expect(self.boot_stop_expression, timeout=1)
-                        shell.sendline(self.boot_stop_sequence)
-                    except pexpect.TIMEOUT:
-                        shell.sendline(self.boot_stop_sequence)
-                    try:
-                        shell.expect(self.bootloader_prompt, timeout=1)
-                        boot_prompt_detected = True
-                        print("Device stopped in bootloader.")
-                        break
-                    except pexpect.TIMEOUT:
-                        continue
-                if not boot_prompt_detected:
-                    raise TimeoutError("Failed to stop device in bootloader within timeout.")
+        shell.sendline(f"setenv noboot 1; setenv replacevol 1; run boot_tftp_production")
+        shell.sendline("")
+        shell.expect("Loading: ")
+        shell.expect("done")
+        shell.expect("blocks erased: OK")
+        shell.expect("blocks written: OK")
+        shell.expect(self.bootloader_prompt)
