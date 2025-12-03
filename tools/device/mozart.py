715d83aa4817e42f8869cbe8125847b0f6a4f871
Lu Dai <lu.dai@mind.be>
2025-12-03 17:31:34 +0100

    simplify the logic
    Signed-off-by: Lu Dai <lu.dai@mind.be>

--- tools/device/mozart.py
+++ tools/device/mozart.py
@@ -13,8 +13,6 @@ import pexpect
 import pexpect.fdpexpect
 import pexpect.pxssh
 from device.prplos import GenericPrplOS
-from device.serial import SerialDevice
-from device.utils import ShellType
 
 
 class Mozart(GenericPrplOS):
@@ -26,7 +24,6 @@ class Mozart(GenericPrplOS):
 
     """The u-boot prompt on the target."""
     bootloader_prompt = r"MT7988> "
-    boot_stop_expression = "ESC to quit"
     boot_stop_sequence = "0"
     bootloader_reboot_command = "run boot_production"
 
@@ -42,36 +39,3 @@ class Mozart(GenericPrplOS):
         shell.expect("blocks erased: OK")
         shell.expect("blocks written: OK")
         shell.expect(self.bootloader_prompt)
-
-    def reboot(self, serial_type: ShellType, stop_in_bootloader: bool = False):
-        with SerialDevice(self.baudrate, self.name,
-                          self.serial_prompt, expect_prompt_on_connect=False) as shell:
-            print("Reset board.")
-
-            if serial_type == ShellType.UBOOT:
-                if stop_in_bootloader:
-                    shell.sendline("reset")
-                else:
-                    shell.sendline(self.bootloader_reboot_command)
-            elif serial_type in [ShellType.PRPLOS, ShellType.RDKB, ShellType.LINUX_UNKNOWN]:
-                shell.sendline("reboot ; sleep 15 && echo force rebooting && reboot -f")
-            if stop_in_bootloader:
-                print("Device will be stopped in its bootloader.")
-                max_wait = 180  # total seconds to wait for boot menu
-                start_time = time.time()
-                boot_prompt_detected = False
-                while time.time() - start_time < max_wait:
-                    try:
-                        shell.expect(self.boot_stop_expression, timeout=1)
-                        shell.sendline(self.boot_stop_sequence)
-                    except pexpect.TIMEOUT:
-                        shell.sendline(self.boot_stop_sequence)
-                    try:
-                        shell.expect(self.bootloader_prompt, timeout=1)
-                        boot_prompt_detected = True
-                        print("Device stopped in bootloader.")
-                        break
-                    except pexpect.TIMEOUT:
-                        continue
-                if not boot_prompt_detected:
-                    raise TimeoutError("Failed to stop device in bootloader within timeout.")
