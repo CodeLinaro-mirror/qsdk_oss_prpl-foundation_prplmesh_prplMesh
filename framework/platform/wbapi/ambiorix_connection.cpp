d4030a9725609e825999f68558ac2c92e924e545
Dmytro Puz <d.puz@inango-systems.com>
2025-12-17 10:10:02 +0100

    framework: wbapi: set usp configuration before amxb_be_load
    Fixes: PPM-3719
    Signed-off-by: Dmytro Puz <d.puz@inango-systems.com>
    Signed-off-by: Houssem Dafdouf <houssem.dafdouf_ext@softathome.com>

--- framework/platform/wbapi/ambiorix_connection.cpp
+++ framework/platform/wbapi/ambiorix_connection.cpp
@@ -47,6 +47,10 @@ bool AmbiorixConnection::init()
     // The purpose is to workaround events issue (PPW-498)
     amxc_var_t *usp_section = amxc_var_add_key(amxc_htable_t, m_config, "usp", NULL);
     amxc_var_add_key(bool, usp_section, "requires-device-prefix", true);
+    // set usp configuration
+    if (amxb_set_config(m_config) != 0) {
+        LOG(ERROR) << "Failed to set amxb config";
+    }
 
     int ret = 0;
     // Load the backend .so file
@@ -55,9 +59,7 @@ bool AmbiorixConnection::init()
         LOG(ERROR) << "Failed to load the " << m_amxb_backend.c_str() << " backend";
         return false;
     }
-    if (amxb_set_config(m_config) != 0) {
-        LOG(ERROR) << "Failed to set amxb config";
-    }
+
     // Connect to the bus
     ret = amxb_connect(&m_bus_ctx, m_bus_uri.c_str());
     if (ret != 0) {
