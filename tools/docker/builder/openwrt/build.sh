37617d167c8fc0ed122824b01d204650f8f237af
Maarten De Decker <maarten.dedecker@mind.be>
2026-02-01 19:47:55 +0100

    DONT MERGE: test UPDK-9.2.4
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -163,9 +163,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-01-29"
-        OPENWRT_TOOLCHAIN_VERSION='6638bc3144d8992adce7f1d9da4723f3a6aa85d0'
-        OPENWRT_VERSION='6638bc3144d8992adce7f1d9da4723f3a6aa85d0'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-02-01 UPDK-9.2.4"
+        OPENWRT_TOOLCHAIN_VERSION='82f6c11814bbdfa9f63094f0a536a01b88acd715'
+        OPENWRT_VERSION='82f6c11814bbdfa9f63094f0a536a01b88acd715'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -204,9 +204,9 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-# prplos latest-24.10_2026-01-29
-OPENWRT_TOOLCHAIN_VERSION='6638bc3144d8992adce7f1d9da4723f3a6aa85d0'
-OPENWRT_VERSION='6638bc3144d8992adce7f1d9da4723f3a6aa85d0'
+# prplos latest-24.10_2026-02-01
+OPENWRT_TOOLCHAIN_VERSION='82f6c11814bbdfa9f63094f0a536a01b88acd715'
+OPENWRT_VERSION='82f6c11814bbdfa9f63094f0a536a01b88acd715'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
