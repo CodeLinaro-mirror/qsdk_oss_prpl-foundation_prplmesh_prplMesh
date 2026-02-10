20965a139abd5c07ad6246b30bf88dae330b5b4d
Lu Dai <lu.dai@mind.be>
2026-02-10 10:43:55 +0100

    ci: upstep prplos latest-24.10 2026.02.10
    prplos hash update on 2026.02.10
    
    Signed-off-by: Lu Dai <lu.dai@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -163,9 +163,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-01-31"
-        OPENWRT_TOOLCHAIN_VERSION='96a0ba279c16da2e1d8282a26050b78cf04da579'
-        OPENWRT_VERSION='96a0ba279c16da2e1d8282a26050b78cf04da579'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-02-10"
+        OPENWRT_TOOLCHAIN_VERSION='257ee69bb7c73ff643e41f24c83f669b9dc786eb'
+        OPENWRT_VERSION='257ee69bb7c73ff643e41f24c83f669b9dc786eb'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -204,9 +204,9 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-# prplos latest-24.10_2026-01-31
-OPENWRT_TOOLCHAIN_VERSION='96a0ba279c16da2e1d8282a26050b78cf04da579'
-OPENWRT_VERSION='96a0ba279c16da2e1d8282a26050b78cf04da579'
+# prplos latest-24.10_2026-02-10
+OPENWRT_TOOLCHAIN_VERSION='257ee69bb7c73ff643e41f24c83f669b9dc786eb'
+OPENWRT_VERSION='257ee69bb7c73ff643e41f24c83f669b9dc786eb'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
