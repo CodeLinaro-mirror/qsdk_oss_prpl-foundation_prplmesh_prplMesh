de769858cf239eed5c85fe6cb174b2a81c7b2759
Maarten De Decker <maarten.dedecker@mind.be>
2026-02-23 15:54:46 +0100

    CI: certification: disable hidden BH SSIDs
    WPS might fail between a WFA CTT device and a DUT with a hidden BH SSID.
    Disable hidden SSIDs in certification tests until this is resolved.
    
    References: PPW-1399
    
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- ci/configuration/certification/R4/freedom.sh
+++ ci/configuration/certification/R4/freedom.sh
@@ -73,6 +73,8 @@ ba-cli "WiFi.Radio.[OperatingFrequencyBand == \"2.4GHz\"].OperatingStandards=\"b
 ba-cli "WiFi.Radio.[OperatingFrequencyBand == \"5GHz\"].OperatingStandards=\"a,n,ac,ax\""
 ba-cli "WiFi.Radio.[OperatingFrequencyBand == \"6GHz\"].OperatingStandards=\"ax\""
 
+# Don't hide the BH AP SSID (PPW-1399)
+ba-cli WiFi.AccessPoint.*.SSIDAdvertisementEnabled=1
 
 # Restrict channel bandwidth or the certification test could miss beacons
 # (see PPM-258)
