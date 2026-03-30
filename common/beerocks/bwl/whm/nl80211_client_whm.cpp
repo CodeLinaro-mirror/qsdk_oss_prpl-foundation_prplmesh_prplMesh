4b7b9fca738ef6e42bd9639462a905334b43daa2
Dmytro Kysil <d.kysil@inango-systems.com>
2026-03-30 13:03:44 +0300

    agent: common: fix link metric response data collecting
    Add separate handling for access point and end points in
     `nl80211_client_whm::get_sta_info()`
    
    Closes: PPM-3769
    Signed-off-by: Dmytro Kysil <d.kysil@inango-systems.com>

--- common/beerocks/bwl/whm/nl80211_client_whm.cpp
+++ common/beerocks/bwl/whm/nl80211_client_whm.cpp
@@ -69,36 +69,43 @@ bool nl80211_client_whm::get_sta_info(const std::string &interface_name,
     if (!m_connection) {
         return false;
     }
-    std::string sta_mac_str = tlvf::mac_to_string(sta_mac_address);
-    std::string assoc_device_path =
-        wbapi_utils::search_path_assocDev_by_mac(interface_name, sta_mac_str);
 
-    auto assoc_device_obj = m_connection->get_object(assoc_device_path, 0, true);
-    if (!assoc_device_obj) {
-        LOG(ERROR) << "failed to get AssociatedDevice object " << assoc_device_path;
+    std::string sta_mac_str = tlvf::mac_to_string(sta_mac_address);
+
+    std::string assoc_device_search_path =
+        wbapi_utils::search_path_assocDev_by_mac(interface_name, sta_mac_str);
+    std::vector<std::string> assoc_device_path_list;
+    m_connection->resolve_path(assoc_device_search_path, assoc_device_path_list);
+    if (assoc_device_path_list.size() == 1) {
+        AmbiorixVariantSmartPtr assoc_device_obj =
+            m_connection->get_object(assoc_device_path_list.front(), 0, true);
+        if (assoc_device_obj) {
+            get_sta_info_from_assoc_device(assoc_device_obj, sta_info);
+            return true;
+        }
+    } else if (!assoc_device_path_list.empty()) {
+        LOG(ERROR) << "Search path resolved to several objects: " << assoc_device_search_path;
         return false;
     }
-    assoc_device_obj->read_child(sta_info.tx_bytes, "TxBytes");
-    assoc_device_obj->read_child(sta_info.rx_bytes, "RxBytes");
-    assoc_device_obj->read_child(sta_info.tx_packets, "TxPacketCount");
-    assoc_device_obj->read_child(sta_info.rx_packets, "RxPacketCount");
-    assoc_device_obj->read_child(sta_info.tx_retries, "Tx_Retransmissions");
-    assoc_device_obj->read_child(sta_info.tx_failed, "TxErrors");
-    assoc_device_obj->read_child(sta_info.signal_dbm, "SignalStrength");
-    assoc_device_obj->read_child(sta_info.signal_avg_dbm, "AvgSignalStrength");
-    uint32_t u32Val;
-    if (assoc_device_obj->read_child(u32Val, "LastDataDownlinkRate")) {
-        sta_info.tx_bitrate_100kbps = u32Val / 100;
-    }
-    if (assoc_device_obj->read_child(u32Val, "LastDataUplinkRate")) {
-        sta_info.rx_bitrate_100kbps = u32Val / 100;
-    }
-    uint32_t dl_bandwidth;
-    assoc_device_obj->read_child(dl_bandwidth, "DownlinkBandwidth");
-    std::string sVal      = std::to_string(dl_bandwidth);
-    sta_info.dl_bandwidth = wbapi_utils::bandwith_from_string(sVal + "MHz");
 
-    return true;
+    std::string end_point_search_path = wbapi_utils::search_path_ep_by_iface(interface_name);
+    std::vector<std::string> end_point_path_list;
+    m_connection->resolve_path(end_point_search_path, end_point_path_list);
+    if (end_point_path_list.size() == 1) {
+        AmbiorixVariantSmartPtr end_point_obj =
+            m_connection->get_object(end_point_path_list.front(), 0, true);
+        if (end_point_obj) {
+            get_sta_info_from_end_point(end_point_obj, sta_info);
+            return true;
+        }
+    } else if (!end_point_path_list.empty()) {
+        LOG(ERROR) << "Search path resolved to several objects: " << end_point_search_path;
+        return false;
+    }
+
+    LOG(ERROR) << "Failed to get both AssociatedDevice and EndPoint objects for iface="
+               << interface_name << " mac=" << sta_mac_address;
+    return false;
 }
 
 bool nl80211_client_whm::get_survey_info(const std::string &interface_name, SurveyInfo &survey_info)
@@ -138,6 +145,56 @@ bool nl80211_client_whm::send_delba(const std::string &interface_name, const sMa
                                     const sMacAddr &src, const sMacAddr &bssid)
 {
     return false;
+}
+
+void nl80211_client_whm::get_sta_info_from_assoc_device(
+    beerocks::wbapi::AmbiorixVariantSmartPtr &assoc_device, sta_info &sta_info)
+{
+    assoc_device->read_child(sta_info.tx_bytes, "TxBytes");
+    assoc_device->read_child(sta_info.rx_bytes, "RxBytes");
+    assoc_device->read_child(sta_info.tx_packets, "TxPacketCount");
+    assoc_device->read_child(sta_info.rx_packets, "RxPacketCount");
+    assoc_device->read_child(sta_info.tx_retries, "Tx_Retransmissions");
+    assoc_device->read_child(sta_info.tx_failed, "TxErrors");
+    assoc_device->read_child(sta_info.signal_dbm, "SignalStrength");
+    assoc_device->read_child(sta_info.signal_avg_dbm, "AvgSignalStrength");
+
+    uint32_t u32Val;
+    if (assoc_device->read_child(u32Val, "LastDataDownlinkRate")) {
+        sta_info.tx_bitrate_100kbps = u32Val / 100;
+    }
+    if (assoc_device->read_child(u32Val, "LastDataUplinkRate")) {
+        sta_info.rx_bitrate_100kbps = u32Val / 100;
+    }
+
+    uint32_t dl_bandwidth;
+    assoc_device->read_child(dl_bandwidth, "DownlinkBandwidth");
+    sta_info.dl_bandwidth = wbapi_utils::bandwith_from_string(std::to_string(dl_bandwidth) + "MHz");
+}
+
+void nl80211_client_whm::get_sta_info_from_end_point(
+    beerocks::wbapi::AmbiorixVariantSmartPtr &end_point, sta_info &sta_info)
+{
+    end_point->read_child(sta_info.tx_bytes, "TxBytes");
+    end_point->read_child(sta_info.rx_bytes, "RxBytes");
+    end_point->read_child(sta_info.tx_packets, "TxPacketCount");
+    end_point->read_child(sta_info.rx_packets, "RxPacketCount");
+    end_point->read_child(sta_info.tx_retries, "Tx_Retransmissions");
+    sta_info.tx_failed = 0;
+    end_point->read_child(sta_info.signal_dbm, "SignalStrength");
+    sta_info.signal_avg_dbm = sta_info.signal_dbm;
+
+    uint32_t u32Val;
+    if (end_point->read_child(u32Val, "LastDataDownlinkRate")) {
+        sta_info.rx_bitrate_100kbps = u32Val / 100;
+    }
+    if (end_point->read_child(u32Val, "LastDataUplinkRate")) {
+        sta_info.tx_bitrate_100kbps = u32Val / 100;
+    }
+
+    std::string link_bandwidth;
+    end_point->read_child(link_bandwidth, "LinkBandwidth");
+    sta_info.dl_bandwidth = wbapi_utils::bandwith_from_string(link_bandwidth);
 }
 
 } // namespace bwl
