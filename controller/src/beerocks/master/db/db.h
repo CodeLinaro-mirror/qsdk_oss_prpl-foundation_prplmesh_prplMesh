f801e33f1a9218bbf450bfbe109e82a3317da127
Andrey Erokhin <a.erokhin@inango-systems.com>
2026-03-31 16:31:37 +0500

    controller: db: add counter with callback utility class
    Closes: PPM-3970
    References: FEAT-319
    Signed-off-by: Andrey Erokhin <a.erokhin@inango-systems.com>

--- controller/src/beerocks/master/db/db.h
+++ controller/src/beerocks/master/db/db.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause-Patent
  *
- * SPDX-FileCopyrightText: 2016-2022 the prplMesh contributors (see AUTHORS.md)
+ * SPDX-FileCopyrightText: 2016-2026 the prplMesh contributors (see AUTHORS.md)
  *
  * This code is subject to the terms of the BSD+Patent license.
  * See LICENSE file for more details.
@@ -69,6 +69,35 @@ namespace son {
 
 // Forward declaration for Controller context saving
 class Controller;
+
+/**
+ * Invokes the \ref callback (if any) once, once the \ref counter reaches zero
+ */
+class single_shot_counter {
+    unsigned counter = 0;
+    std::function<void()> callback;
+
+public:
+    single_shot_counter() = default;
+    single_shot_counter(unsigned initial, std::function<void()> f)
+        : counter(initial), callback(std::move(f))
+    {
+    }
+
+    ~single_shot_counter()                               = default;
+    single_shot_counter(single_shot_counter &&) noexcept = default;
+    single_shot_counter &operator=(single_shot_counter &&) noexcept = default;
+
+    single_shot_counter(const single_shot_counter &) = delete;
+    single_shot_counter &operator=(const single_shot_counter &) = delete;
+
+    /** Increase counter and return the previous value. */
+    unsigned count_up();
+    /** Decrease counter and return the previous value; trigger callback once on zero. */
+    unsigned count_down();
+    /** True while the callback is still armed and the counter is non-zero. */
+    explicit operator bool() const { return counter && callback; }
+};
 
 class db {
 
