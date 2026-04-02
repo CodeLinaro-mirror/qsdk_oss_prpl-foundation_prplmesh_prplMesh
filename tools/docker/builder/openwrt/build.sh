54bbfa56e7dc3344d65cf54a78d4f78fb4e27b77
Lu Dai <lu.dai@mind.be>
2026-04-02 20:20:12 +0200

    test
    Signed-off-by: Lu Dai <lu.dai@mind.be>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -163,9 +163,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-03-26"
-        OPENWRT_TOOLCHAIN_VERSION='8048d7eee62626162bf879dbcdca073ac441cb7a'
-        OPENWRT_VERSION='8048d7eee62626162bf879dbcdca073ac441cb7a'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS mr 2453"
+        OPENWRT_TOOLCHAIN_VERSION='3139f73c54fb682d8281654c2dbada62d7fe7856'
+        OPENWRT_VERSION='3139f73c54fb682d8281654c2dbada62d7fe7856'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -204,9 +204,9 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-# prplos latest-24.10_2026-03-26
-OPENWRT_TOOLCHAIN_VERSION='8048d7eee62626162bf879dbcdca073ac441cb7a'
-OPENWRT_VERSION='8048d7eee62626162bf879dbcdca073ac441cb7a'
+# prplos mr 2453
+OPENWRT_TOOLCHAIN_VERSION='3139f73c54fb682d8281654c2dbada62d7fe7856'
+OPENWRT_VERSION='3139f73c54fb682d8281654c2dbada62d7fe7856'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
