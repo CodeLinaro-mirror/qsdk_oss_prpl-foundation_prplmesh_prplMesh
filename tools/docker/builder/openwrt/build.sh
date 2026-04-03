996f9f2330a272fd6fde810dd306dd320e870e49
Cihangir Odabas <cihangir.odabas@prplfoundation.org>
2026-04-03 16:55:19 +0300

    CI: bump prplOS version to latest-24.10
    ath13.1csu1 SDK update
    prplware-5 preparation
    
    Signed-off-by: Cihangir Odabas <cihangir.odabas@prplfoundation.org>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -163,9 +163,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-03-26"
-        OPENWRT_TOOLCHAIN_VERSION='8048d7eee62626162bf879dbcdca073ac441cb7a'
-        OPENWRT_VERSION='8048d7eee62626162bf879dbcdca073ac441cb7a'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-04-03"
+        OPENWRT_TOOLCHAIN_VERSION='cd4c4f608a1b98b39046bcbca3b4aa0d13d17710'
+        OPENWRT_VERSION='cd4c4f608a1b98b39046bcbca3b4aa0d13d17710'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -204,9 +204,9 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-# prplos latest-24.10_2026-03-26
-OPENWRT_TOOLCHAIN_VERSION='8048d7eee62626162bf879dbcdca073ac441cb7a'
-OPENWRT_VERSION='8048d7eee62626162bf879dbcdca073ac441cb7a'
+# prplos latest-24.10_2026-04-03
+OPENWRT_TOOLCHAIN_VERSION='cd4c4f608a1b98b39046bcbca3b4aa0d13d17710'
+OPENWRT_VERSION='cd4c4f608a1b98b39046bcbca3b4aa0d13d17710'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
