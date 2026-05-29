0439ef9ed0a70b40fe72be9b83572b22e12ba0e2
Ahmed JARRAYA <ahmed.jarraya@softathome.com>
2026-05-29 15:44:17 +0200

    test: bridge: Disable STP
    Signed-off-by: Ahmed JARRAYA <ahmed.jarraya@softathome.com>

--- ci/configuration/certification/R4/freedom.sh
+++ ci/configuration/certification/R4/freedom.sh
@@ -45,6 +45,8 @@ ba-cli IP.Interface.wan.IPv4Enable=1
 ba-cli "IP.Interface.[Name == \"br-lan\"].IPv4Address.lan.IPAddress=192.165.100.150"
 ba-cli "IP.Interface.[Name == \"br-guest\"].IPv4Address.[Alias == \"guest\"].IPAddress=192.165.200.150"
 
+ba-cli "Bridging.Bridge.[Alias == \"lan\"].STP.Enable=0"
+
 # The backhaulWireInterface might not be UP and in br-lan, if previous test was using wifi backhaul (PPM-3361)
 ba-cli "Bridging.Bridge.[Alias == \"lan\"].Port.[Name == \"lan4\"].Enable=0"
 ba-cli "Device.Ethernet.Interface.[Name == \"lan4\"].Enable=0"
