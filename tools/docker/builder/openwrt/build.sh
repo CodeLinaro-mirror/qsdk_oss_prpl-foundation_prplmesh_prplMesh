6c824ae4f43c04438abb296fa102c6cfd9feed46
Cihangir Odabas <cihangir.odabas@prplfoundation.org>
2026-06-08 15:55:43 +0300

    CI: bump prplOS version to latest-24.10
    add QCA's MBSSID improvement
    
    Signed-off-by: Cihangir Odabas <cihangir.odabas@prplfoundation.org>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -163,9 +163,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-06-05"
-        OPENWRT_TOOLCHAIN_VERSION='c73a23a6abe8255bc7ad107724daa48db039c50d'
-        OPENWRT_VERSION='c73a23a6abe8255bc7ad107724daa48db039c50d'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-06-08"
+        OPENWRT_TOOLCHAIN_VERSION='737a779b93be4cf3aab6512d36e2695fa0314702'
+        OPENWRT_VERSION='737a779b93be4cf3aab6512d36e2695fa0314702'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -204,9 +204,9 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-# prplos latest-24.10_2026-06-05
-OPENWRT_TOOLCHAIN_VERSION='c73a23a6abe8255bc7ad107724daa48db039c50d'
-OPENWRT_VERSION='c73a23a6abe8255bc7ad107724daa48db039c50d'
+# prplos latest-24.10_2026-06-08
+OPENWRT_TOOLCHAIN_VERSION='737a779b93be4cf3aab6512d36e2695fa0314702'
+OPENWRT_VERSION='737a779b93be4cf3aab6512d36e2695fa0314702'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
