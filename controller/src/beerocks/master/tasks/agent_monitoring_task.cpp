6b0a5a6704de693e215743d81fd4d3ae38a8e627
Kannan Periyasamy <kannan.p@tataelxsi.co.in>
2026-06-10 12:51:19 +0530

    Configuration Templates offer a flexible and centralized approach to Wi-Fi configuration. They can be reused across multiple deployments within an operator’s network and applied to multiple APs, radios, and BSSs in multi-AP environments. These templates are defined independently of network topology and can support a wide range of scenarios by selecting templates based on AP or radio capabilities. Additionally, different BSSs can be configured on different radios to deliver varied service types.
    Closes PPM-3843
    
    Signed-off-by: Kannan Periyasamy <kannan.p@tataelxsi.co.in>

--- controller/src/beerocks/master/tasks/agent_monitoring_task.cpp
+++ controller/src/beerocks/master/tasks/agent_monitoring_task.cpp
@@ -11,6 +11,7 @@
 #include "../son_actions.h"
 
 #include <bcl/beerocks_string_utils.h>
+#include <bcl/network/network_utils.h>
 #include <beerocks/tlvf/beerocks_message_control.h>
 #include <bpl/bpl_cfg.h>
 #include <easylogging++.h>
@@ -34,6 +35,9 @@
 #include <string>
 #include <utility>
 #include <vector>
+#ifdef ENABLE_NBAPI
+#include "on_action.h"
+#endif
 
 using namespace beerocks;
 using namespace net;
@@ -276,6 +280,9 @@ agent_monitoring_task::agent_monitoring_task(db &database_, ieee1905_1::CmduMess
 
 void agent_monitoring_task::work()
 {
+#ifdef ENABLE_NBAPI
+    prplmesh::controller::actions::templates_commit_apply_pending();
+#endif
     if (!m_ap_autoconfig_renew_sent) {
         /**
          * Usually, the ap-configuration sequence is started when the agent sends the ap-config
@@ -473,6 +480,12 @@ void agent_monitoring_task::handle_event(int event_type, void *obj)
         }
         break;
     }
+#ifdef ENABLE_NBAPI
+    case (TEMPLATES_COMMIT_APPLY): {
+        prplmesh::controller::actions::templates_commit_apply_pending();
+        break;
+    }
+#endif
     default:
         break;
     }
@@ -562,6 +575,11 @@ bool agent_monitoring_task::start_agent_monitoring(const sMacAddr &src_mac,
         LOG(ERROR) << "Failed to add " << agent_connected_event_path << ".Neighbor";
         return false;
     }
+#ifdef ENABLE_NBAPI
+    if (database.config.use_dataelements_vap_configs) {
+        prplmesh::controller::actions::templates_restage_only();
+    }
+#endif
     if (database.get_agent_monitoring_task_id() == db::TASK_ID_NOT_FOUND) {
         database.assign_agent_monitoring_task_id(id);
     }
