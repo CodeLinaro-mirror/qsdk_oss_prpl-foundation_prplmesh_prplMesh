9395cd20e117851eff4d4799a79f12629592cae9
Lu Dai <lu.dai@mind.be>
2026-06-16 14:40:21 +0200

    ci: remove wireless credential before starting test
    when DUT finish wireless onboarding, credentials will be stored
    in the next run of wired onboarding, after backhual interface enabled and radio enabled,
    and before ucc send out “dut reset“, a loop is constructed by DUT&extender&controller
    
    Closes: PPM-4118
    Signed-off-by: Lu Dai <lu.dai@mind.be>

--- ci/configuration/certification/R1/freedom.sh
+++ ci/configuration/certification/R1/freedom.sh
@@ -43,6 +43,8 @@ ba-cli IP.Interface.wan.IPv4Enable=1
 ba-cli "IP.Interface.[Name == \"br-lan\"].IPv4Address.lan.IPAddress=192.165.100.150"
 
 # The backhaulWireInterface might not be UP and in br-lan, if previous test was using wifi backhaul (PPM-3361)
+# Remove wireless backhual credentials before enable wired backhual, if previous test used wifi backhaul (PPM-4118)
+ba-cli "WiFi.EndPoint.*.Profile.*.-"
 ba-cli "Bridging.Bridge.[Alias == \"lan\"].Port.[Name == \"lan4\"].Enable=0"
 ba-cli "Device.Ethernet.Interface.[Name == \"lan4\"].Enable=0"
 ba-cli "Device.Ethernet.Interface.[Name == \"lan4\"].Enable=1"
--- ci/configuration/certification/R6/freedom.sh
+++ ci/configuration/certification/R6/freedom.sh
@@ -46,6 +46,8 @@ ba-cli "IP.Interface.[Name == \"br-lan\"].IPv4Address.[Alias == \"lan\"].IPAddre
 ba-cli "IP.Interface.[Name == \"br-guest\"].IPv4Address.[Alias == \"guest\"].IPAddress=192.165.200.150"
 
 # The backhaulWireInterface might not be UP and in br-lan, if previous test was using wifi backhaul (PPM-3361)
+# Remove wireless backhual credentials before enable wired backhual, if previous test used wifi backhaul (PPM-4118)
+ba-cli "WiFi.EndPoint.*.Profile.*.-"
 ba-cli "Bridging.Bridge.[Alias == \"lan\"].Port.[Name == \"lan1\"].Enable=0"
 ba-cli "Device.Ethernet.Interface.[Name == \"lan1\"].Enable=0"
 ba-cli "Device.Ethernet.Interface.[Name == \"lan1\"].Enable=1"
