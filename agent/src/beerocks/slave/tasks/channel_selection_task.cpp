2c5162360bf52e0ac3bf5620754c5748fd397089
Dmytro Kysil <d.kysil@inango-systems.com>
2026-06-16 15:27:56 +0300

    agent: fix issues reported by coverity
    Closes: PPM-3838
    Signed-off-by: Dmytro Kysil <d.kysil@inango-systems.com>

--- agent/src/beerocks/slave/tasks/channel_selection_task.cpp
+++ agent/src/beerocks/slave/tasks/channel_selection_task.cpp
@@ -2919,12 +2919,12 @@ bool ChannelSelectionTask::add_eht_operations_tlv(ieee1905_1::CmduMessageTx &cmd
     eht_operations_bss->disabled_subchannel_bitmap() =
         request_info.eht_params.disabled_subchannel_bitmap;
 
-    if (!eht_operations_radio->add_bss_entries(eht_operations_bss)) {
+    if (!eht_operations_radio->add_bss_entries(std::move(eht_operations_bss))) {
         LOG(ERROR) << "Failed adding BSS entry in eht operation TLV for bssid "
                    << request_info.bssid;
         return false;
     }
-    if (!eht_operation_tlv->add_radio_entries(eht_operations_radio)) {
+    if (!eht_operation_tlv->add_radio_entries(std::move(eht_operations_radio))) {
         LOG(ERROR) << "Failed adding Radio entry in eht operation TLV for mac " << ruid;
         return false;
     }
