e7d3b28624c32689db1bb822a75e701ce3a47665
Akash Balasubramaniyam <akash.d2@tataelxsi.co.in>
2026-06-19 14:47:31 +0530

    Fix pwhm hal issues
    Signed-off-by: Akash Balasubramaniyam <akash.d2@tataelxsi.co.in>

--- common/beerocks/bwl/whm/ap_wlan_hal_whm.cpp
+++ common/beerocks/bwl/whm/ap_wlan_hal_whm.cpp
@@ -2871,7 +2871,7 @@ bool ap_wlan_hal_whm::dpp_bootstrap_gen(const std::string &bootstrap_params, uin
         return false;
     }
 
-    AmbiorixVariant args, result;
+    AmbiorixVariant result;
     AmbiorixVariant args(AMXC_VAR_ID_HTABLE);
     args.add_child("params", bootstrap_params);
 
--- controller/src/beerocks/master/controller.cpp
+++ controller/src/beerocks/master/controller.cpp
@@ -82,6 +82,7 @@
 #include <tlvf/wfa_map/tlvClientInfo.h>
 #include <tlvf/wfa_map/tlvControllerCapability.h>
 #include <tlvf/wfa_map/tlvDeviceInventory.h>
+#include <tlvf/wfa_map/tlvDppBootstrappingUriNotification.h>
 #include <tlvf/wfa_map/tlvDscpMappingTable.h>
 #include <tlvf/wfa_map/tlvEHTOperations.h>
 #include <tlvf/wfa_map/tlvErrorCode.h>
@@ -119,9 +120,6 @@
 #include "../../../vbss/vbss_actions.h"
 #include "../../../vbss/vbss_task.h"
 #endif
-#include <tlvf/wfa_map/tlvDppBootstrappingUriNotification.h>
-// #include <bcl/beerocks_string_utils.h>
-#include "beerocks_string_utils.h"
 namespace son {
 
 namespace {
