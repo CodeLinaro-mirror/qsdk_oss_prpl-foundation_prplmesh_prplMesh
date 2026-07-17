e75041ecb167af92a337e57221ecfd40f9890cfb
Igor Plesser <i.plesser@inango-systems.com>
2026-07-17 07:01:35 +0300

    hal: whm: migrate to KeyPassphraseVisible
    Replace "KeyPassPhrase" and "SAEPassphrase" with
    "KeyPassphraseVisible" and "SAEPassphraseVisible"
    respectively when accessing pwhm.
    
    References: PPW-891
    Signed-off-by: Igor Plesser <i.plesser@inango-systems.com>

--- common/beerocks/bwl/whm/sta_wlan_hal_whm.cpp
+++ common/beerocks/bwl/whm/sta_wlan_hal_whm.cpp
@@ -850,7 +850,7 @@ bool sta_wlan_hal_whm::set_profile_params(const Profile &profile)
     // Configure Profile.Security object
     params.set_type(AMXC_VAR_ID_HTABLE);
     params.add_child("ModeEnabled", "WPA3-Personal-Transition");
-    params.add_child("KeyPassPhrase", profile.pass);
+    params.add_child("KeyPassphrase", profile.pass);
     params.add_child("SAEPassphrase", profile.pass);
     params.add_child("MFPConfig", "Optional");
     ret = m_ambiorix_cl.update_object(profile_security_path, params);
@@ -1161,7 +1161,7 @@ bool sta_wlan_hal_whm::process_ep_wps_event(const std::string &interface,
     if (reason == "Success") {
         std::string ssid, key, mode;
         data->read_child(ssid, "SSID");
-        data->read_child(key, "KeyPassPhrase");
+        data->read_child(key, "KeyPassphraseVisible");
         data->read_child(mode, "securitymode");
         if (ssid.empty() || key.empty() || mode.empty()) {
             return false;
