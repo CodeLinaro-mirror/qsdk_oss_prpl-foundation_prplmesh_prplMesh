732376bad38b63d5e3fe6ae1bf87a36e87cc8003
Artem Trokhymchuk <a.trokhymchuk@inango-systems.com>
2026-07-28 13:55:47 +0300

    ci: enable support for Cobertura XML report generation
    Enable support generating Cobertura XML report for per-line code
    coverage in the MR.
    
    References: PCF-3495
    Signed-off-by: Artem Trokhymchuk <a.trokhymchuk@inango-systems.com>

--- coverage.sh
+++ coverage.sh
@@ -6,11 +6,16 @@ cd "${0%/*}" || { echo "Couldn't cd to ${0%/*}!"; exit 1; }
 export REPORTS_DIR=./build/reports
 export COVERAGE_DIR=${REPORTS_DIR}/coverage
 
-rm -rf ${COVERAGE_DIR} 
+rm -rf ${COVERAGE_DIR}
 mkdir -p ${COVERAGE_DIR}
 
 # See "Gcovr User Guide" for details on 'gcovr' usage
 # https://gcovr.com/en/stable/guide.html
-gcovr -r . --exclude='build/*' --exclude='framework/external/*' --exclude='.*/unit_tests/.*' --html --html-details -o ${COVERAGE_DIR}/coverage.html
+GCOVR_EXCLUDES="--exclude=build/* --exclude=framework/external/* --exclude=.*/(unit_)?tests?/.*"
 
-
+# shellcheck disable=SC2086
+gcovr -r . $GCOVR_EXCLUDES -s -x -o ${COVERAGE_DIR}/cobertura.xml
+# shellcheck disable=SC2086
+gcovr -r . $GCOVR_EXCLUDES --html --html-details -o ${COVERAGE_DIR}/coverage.html
+# shellcheck disable=SC2086
+gcovr -r . $GCOVR_EXCLUDES | tee ${COVERAGE_DIR}/gcovr_summary.txt
