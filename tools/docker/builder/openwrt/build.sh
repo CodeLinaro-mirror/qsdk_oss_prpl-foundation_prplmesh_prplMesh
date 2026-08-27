89039dd9f6ce7dfe31457d3456b24109a59fdecc
Cihangir Odabas <cihangir.odabas@prplfoundation.org>
2026-08-27 12:11:10 +0300

    CI: bump prplOS version to ath14.0-bsta fix
    Signed-off-by: Cihangir Odabas <cihangir.odabas@prplfoundation.org>

--- tools/docker/builder/openwrt/build.sh
+++ tools/docker/builder/openwrt/build.sh
@@ -163,9 +163,9 @@ main() {
         OPENWRT_TOOLCHAIN_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
         OPENWRT_VERSION='21169344e223c5e02e8afedc3cc5648acd42f6cc'
     else
-        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_2026-09-15"
-        OPENWRT_TOOLCHAIN_VERSION='29c7511676def07c57e5d536e507902a8d855735'
-        OPENWRT_VERSION='29c7511676def07c57e5d536e507902a8d855735'
+        dbg "$TARGET_DEVICE non-legacy platform, building on prplOS latest-24.10_ath14-bsta-fix"
+        OPENWRT_TOOLCHAIN_VERSION='d18b8edd11724fe8dbffe96609c517f88acb6fe5'
+        OPENWRT_VERSION='d18b8edd11724fe8dbffe96609c517f88acb6fe5'
     fi
 
     dbg "OPENWRT_REPOSITORY=$OPENWRT_REPOSITORY"
@@ -204,9 +204,9 @@ main() {
 VERBOSE=false
 IMAGE_ONLY=false
 OPENWRT_REPOSITORY='https://gitlab.com/prpl-foundation/prplos/prplos.git'
-# prplos latest-24.10_2026-09-15
-OPENWRT_TOOLCHAIN_VERSION='29c7511676def07c57e5d536e507902a8d855735'
-OPENWRT_VERSION='29c7511676def07c57e5d536e507902a8d855735'
+# prplos latest-24.10_ath14-bsta-fix
+OPENWRT_TOOLCHAIN_VERSION='d18b8edd11724fe8dbffe96609c517f88acb6fe5'
+OPENWRT_VERSION='d18b8edd11724fe8dbffe96609c517f88acb6fe5'
 PRPLMESH_VARIANT="-nl80211"
 DOCKER_TARGET_STAGE="prplmesh-builder"
 SHELL_ONLY=false
